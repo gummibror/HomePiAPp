7d92bee45d8cd709231d516e78ab566696a6de2c
gummibror <bjorni_arvid_95@hotmail.com>
2025-04-22 23:20:25 +0200

    försöker lösa pinsen

--- server/controllers/gpioController.js
+++ server/controllers/gpioController.js
@@ -2,11 +2,12 @@
 const { Gpio } = require('onoff');
 
 const turnOnPin = (req, res) => {
-    const { pin } = req.body;
+    const pin = parseInt(req.body.pin, 10);
   
     try {
       const gpio = new Gpio(pin, 'out');
       gpio.writeSync(1); // 1 = HIGH (turn on)
+      gpio.unexport();
       res.json({ success: true, message: `Pin ${pin} turned ON` });
     } catch (err) {
       console.error(`Failed to turn on pin ${pin}:`, err.message);
@@ -15,11 +16,12 @@ const turnOnPin = (req, res) => {
   };
   
   const turnOffPin = (req, res) => {
-    const { pin } = req.body;
+    const pin = parseInt(req.body.pin, 10);
   
     try {
       const gpio = new Gpio(pin, 'out');
       gpio.writeSync(0); // 0 = LOW (turn off)
+      gpio.unexport();
       res.json({ success: true, message: `Pin ${pin} turned OFF` });
     } catch (err) {
       console.error(`Failed to turn off pin ${pin}:`, err.message);
